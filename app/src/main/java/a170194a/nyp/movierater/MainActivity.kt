283c9d0ac252f75619826cfa6565b982c619af32
Chow Jia Jun <170194A@mymail.nyp.edu.sg>
2018-11-06 20:35:14 +0800

    Halfway done

--- app/src/main/java/a170194a/nyp/movierater/MainActivity.kt
+++ app/src/main/java/a170194a/nyp/movierater/MainActivity.kt
@@ -2,6 +2,8 @@ package a170194a.nyp.movierater
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,4 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun validateForm(v: View) {
+        if (etMovieName.text.toString() == "" || etMovieDescription.text.toString() == "") {
+            if (etMovieName.text.toString() == "") {
+                etMovieName.setError("This field cannot be empty")
+            }
+
+            if (etMovieDescription.text.toString() == "") {
+                etMovieDescription.setError("This section cannot be empty")
+            }
+        }
+    }
+
+    fun showTextBox(v: View) {
+        if (chkbxSuitable.isChecked) {
+            llotherBoxes.visibility = View.INVISIBLE
+        }
+    }
 }
